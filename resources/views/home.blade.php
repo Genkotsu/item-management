071766108ad3f44709482871364e85b13a7081bf
Genkotsu <chakimaru2@gmail.com>
2022-10-17 22:02:25 +0900

    home変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 @section('content_header')
     <h1>商品一覧</h1>
     <div>
-    div style="text-align: center">     -->
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </div>
     <br>
