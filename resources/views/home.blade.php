dbf7e2d0b4c10b2468d0e7b1d10a497f5b8dd627
Genkotsu <chakimaru2@gmail.com>
2022-10-18 19:43:50 +0900

    home画像変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,11 +9,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </div>
     <br>
-    <img src="/img/home.jpg" alt="home画像">
+    <img src="/img/home.jpg" alt="home画像" height="700px" width="1500px">
 @stop
 
 @section('content')
-    <p>秋のお勧め商品です</p>
+    <h2>秋のお勧め商品です</h2>
 @stop
 @section('css')
     {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
